a94cc826be3aec430feb72e3ae2316e6c1bed7d5
PHUCDAT @DM <bluehairmylove2014@gmail.com>
2023-06-20 20:34:49 +0700

    fix CORS 1.0

--- server.js
+++ server.js
@@ -3,16 +3,17 @@ const cors = require("cors");
 const bodyParser = require('body-parser');
 
 const app = express();
-
-app.use(bodyParser.json());
-
 app.use(express.json());
 
+// CORS config
 const corsOptions = { origin: false };
 app.use(cors(corsOptions));
 
+// Body parser
+app.use(bodyParser.json());
+
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://www.sunrise-continent.online/");
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
